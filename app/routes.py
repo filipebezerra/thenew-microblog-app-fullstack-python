7fea631e8fe1862345cbcd6995825b88b97d1a58
Filipe Bezerra <filipebzerra@gmail.com>
2020-06-16 18:06:30 -0300

    Include templates instead of string HTML.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,34 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Filipe'}
+    posts = [
+        {
+            'author': {'username': 'Marilyn'},
+            'body': 'I\'m having good times at the beach'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'I want to give you a tip about travelling to Paris'
+        },
+        {
+            'author': {'username': 'Jake'},
+            'body': 'What\'s going on right now at Democratic Republic of Congo'
+        },
+        {
+            'author': {'username': 'Frank'},
+            'body': 'We need your donation right now'
+        },
+        {
+            'author': {'username': 'Natalie'},
+            'body': 'Give love to everyone'
+        },
+        {
+            'author': {'username': 'Joanna'},
+            'body': 'We need to talk about Impostor Syndrome'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
--- microblog.py
+++ microblog.py
@@ -1,1 +1,9 @@
 from app import app
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        use_reloader=False,
+        use_debugger=False,
+        passthrough_errors=True
+    )
