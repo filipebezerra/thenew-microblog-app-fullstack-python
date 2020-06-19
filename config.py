19d3afd58d0b97d7634346333a994e0324434e5a
Filipe Bezerra <filipebzerra@gmail.com>
2020-06-18 22:53:36 -0300

    Include database models and migrations

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY') or 'dev-microblog-sec-key'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
+        f'sqlite:///{os.path.join(basedir, "app.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
