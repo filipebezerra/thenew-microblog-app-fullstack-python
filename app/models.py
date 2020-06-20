75dbeb2ef0a8a43cfc058f8229ff4e9e51c95467
Filipe Bezerra <filipebzerra@gmail.com>
2020-06-20 15:58:15 -0300

    Include user login

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db
+from flask_login.mixins import UserMixin
+from app import db, login
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -11,6 +14,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def set_password(self, passwod):
+        self.password_hash = generate_password_hash(passwod, salt_length=14)
+
+    def check_password(self, passwod):
+        return check_password_hash(self.password_hash, passwod)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
