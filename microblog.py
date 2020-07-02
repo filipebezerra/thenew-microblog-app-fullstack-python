550b1fd5c5abce0e4a900bf39f4e6f7ccba9cb66
Filipe Bezerra <filipebzerra@gmail.com>
2020-07-02 11:42:49 -0300

    UTC timestamps using Moment

--- microblog.py
+++ microblog.py
@@ -1,15 +1,17 @@
 from app import app
 from app.models import db, User, Post
 
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
 
+
 if __name__ == "__main__":
     app.run(
-        debug=False,
-        use_reloader=False,
-        use_debugger=False,
+        # debug=False,
+        # use_reloader=False,
+        # use_debugger=False,
         passthrough_errors=True,
         host='0.0.0.0'
     )
