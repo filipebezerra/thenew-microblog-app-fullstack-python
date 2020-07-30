c6eec35b45eb2206d4880ada5163243efde409ea
Filipe Bezerra <filipebzerra@gmail.com>
2020-07-30 15:39:09 -0300

    Fix sending email without checking if mail server is configured

--- app/email.py
+++ app/email.py
@@ -10,7 +10,11 @@ def send_async_email(app, msg):
 
 
 def send_email(sender, recipients, subject, text_body, html_body):
-    msg = Message(sender=sender, recipients=recipients,
-                  subject=subject, body=text_body,  html=html_body)
-    Thread(target=send_async_email,
-           args=(current_app._get_current_object(), msg)).start()
+    if current_app.config['MAIL_SERVER']:
+        msg = Message(sender=sender, recipients=recipients,
+                      subject=subject, body=text_body,  html=html_body)
+        Thread(target=send_async_email,
+               args=(current_app._get_current_object(), msg)).start()
+    else:
+        current_app.logger.info('Application not sending emails. '
+                                'Reason: Mail server isn\'t configured.')
